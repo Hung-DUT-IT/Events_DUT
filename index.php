b03cfd9e877c7cfeb24177e24e25c1b2d93b35af
HungCODER1 <huuhung3008@gmail.com>
2022-11-09 00:01:56 +0700

    LayoutEdit
    Redesign Layout

---
+++ index.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <title>DUT Events</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta content="A fully featured admin theme which can be used to build CRM, CMS, etc." name="description">
+        <meta content="Coderthemes" name="author">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <!-- App favicon -->
+        <link rel="shortcut icon" href="assets\images\favicon.ico">
+
+        <!-- plugin css -->
+        <link href="assets\libs\jquery-vectormap\jquery-jvectormap-1.2.2.css" rel="stylesheet" type="text/css">
+
+        <!-- App css -->
+        <link href="assets\css\bootstrap.min.css" rel="stylesheet" type="text/css">
+        <link href="assets\css\icons.min.css" rel="stylesheet" type="text/css">
+        <link href="assets\css\app.min.css" rel="stylesheet" type="text/css">
+
+    </head>
+
+    <body>
+
+        <!-- Begin page -->
+        <div id="wrapper">
+            <?php
+                include "pages/header.php" ;            // <!-- Topbar Start -->
+                include "pages/leftsidebar.php"         // <!-- ========== Left Sidebar Start ========== -->
+            ?>
+            <div class="content-page">
+                <?php
+                    include "pages/main.php" ;      // <!-- content -->
+                    include "pages/footer.php"      // <!-- Footer Start -->
+                ?>
+            </div>
+
+            <!-- ============================================================== -->
+            <!-- End Page content -->
+            <!-- ============================================================== -->
+
+
+        </div>
+        <!-- END wrapper -->
+
+        <!-- Vendor js -->
+        <script src="assets\js\vendor.min.js"></script>
+
+        <script src="assets\libs\apexcharts\apexcharts.min.js"></script>
+        <script src="assets\libs\jquery-sparkline\jquery.sparkline.min.js"></script>
+        <script src="assets\libs\jquery-vectormap\jquery-jvectormap-1.2.2.min.js"></script>
+        <script src="assets\libs\jquery-vectormap\jquery-jvectormap-world-mill-en.js"></script>
+
+        <!-- Peity chart-->
+        <script src="assets\libs\peity\jquery.peity.min.js"></script>
+
+        <!-- init js -->
+        <script src="assets\js\pages\dashboard-2.init.js"></script>
+
+        <!-- App js -->
+        <script src="assets\js\app.min.js"></script>
+        
+    </body>
+</html>
---
+++ pages/header.php
@@ -0,0 +1,118 @@
+<div class="navbar-custom">
+    <ul class="list-unstyled topnav-menu float-right mb-0">
+
+        <li class="d-none d-sm-block">
+            <form class="app-search">
+                <div class="app-search-box">
+                    <div class="input-group">
+                        <input type="text" class="form-control" placeholder="Search...">
+                        <div class="input-group-append">
+                            <button class="btn" type="submit">
+                                <i class="fe-search"></i>
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </form>
+        </li>
+
+        <li class="dropdown notification-list">
+            <a class="nav-link dropdown-toggle  waves-effect waves-light" data-toggle="dropdown" href="#" role="button" aria-haspopup="false" aria-expanded="false">
+                <i class="fe-bell noti-icon"></i>
+                <span class="badge badge-danger rounded-circle noti-icon-badge">4</span>
+            </a>
+            <div class="dropdown-menu dropdown-menu-right dropdown-lg">
+
+                <!-- item-->
+                <div class="dropdown-item noti-title">
+                    <h5 class="m-0">
+                        <span class="float-right">
+                            <a href="" class="text-dark">
+                                <small>Clear All</small>
+                            </a>
+                        </span>Notification
+                    </h5>
+                </div>
+
+                <div class="slimscroll noti-scroll">
+
+                    <!-- item-->
+                    <a href="javascript:void(0);" class="dropdown-item notify-item active">
+                        <div class="notify-icon bg-soft-primary text-primary">
+                            <i class="mdi mdi-comment-account-outline"></i>
+                        </div>
+                        <p class="notify-details">Doug Dukes commented on Admin Dashboard
+                            <small class="text-muted">1 min ago</small>
+                        </p>
+                    </a>
+
+                </div>
+
+                <!-- All-->
+                <a href="javascript:void(0);" class="dropdown-item text-center text-primary notify-item notify-all">
+                    View all
+                    <i class="fi-arrow-right"></i>
+                </a>
+
+            </div>
+        </li>
+
+        <li class="dropdown notification-list">
+            <a class="nav-link dropdown-toggle nav-user mr-0 waves-effect waves-light" data-toggle="dropdown" href="#" role="button" aria-haspopup="false" aria-expanded="false">
+                <img src="assets\images\users\avatar-1.jpg" alt="user-image" class="rounded-circle">
+                <span class="pro-user-name ml-1">
+                    Nik Patel <i class="mdi mdi-chevron-down"></i> 
+                </span>
+            </a>
+            <div class="dropdown-menu dropdown-menu-right profile-dropdown ">
+                <!-- item-->
+                <div class="dropdown-header noti-title">
+                    <h6 class="text-overflow m-0">Welcome !</h6>
+                </div>
+
+                <!-- item-->
+                <a href="javascript:void(0);" class="dropdown-item notify-item">
+                    <i class="remixicon-account-circle-line"></i>
+                    <span>My Account</span>
+                </a>
+
+                <!-- item-->
+                <a href="javascript:void(0);" class="dropdown-item notify-item">
+                    <i class="remixicon-settings-3-line"></i>
+                    <span>Settings</span>
+                </a>
+
+                <div class="dropdown-divider"></div>
+
+                <!-- item-->
+                <a href="javascript:void(0);" class="dropdown-item notify-item">
+                    <i class="remixicon-logout-box-line"></i>
+                    <span>Logout</span>
+                </a>
+
+            </div>
+        </li>
+    </ul>
+
+    <!-- LOGO -->
+    <div class="logo-box">
+        <a href="index.html" class="logo text-center">
+            <span class="logo-lg">
+                <img src="assets\images\logo-light.png" alt="" height="20">
+                <!-- <span class="logo-lg-text-light">Xeria</span> -->
+            </span>
+            <span class="logo-sm">
+                <!-- <span class="logo-sm-text-dark">X</span> -->
+                <img src="assets\images\logo-sm.png" alt="" height="24">
+            </span>
+        </a>
+    </div>
+
+    <ul class="list-unstyled topnav-menu topnav-menu-left m-0">
+        <li>
+            <button class="button-menu-mobile waves-effect waves-light">
+                <i class="fe-menu"></i>
+            </button>
+        </li>
+    </ul>
+</div>
